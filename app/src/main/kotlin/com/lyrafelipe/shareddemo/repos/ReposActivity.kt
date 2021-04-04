0230db23d8eb592eb8bf44746bcae439716cb27f
lyrafelipe <appslyraf@gmail.com>
2021-04-04 09:02:44 -0300

    add repos layouts

--- app/src/main/kotlin/com/lyrafelipe/shareddemo/repos/ReposActivity.kt
+++ app/src/main/kotlin/com/lyrafelipe/shareddemo/repos/ReposActivity.kt
@@ -1,11 +1,13 @@
-package com.lyrafelipe.shareddemo
+package com.lyrafelipe.shareddemo.repos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lyrafelipe.shareddemo.R
 
-class MainActivity : AppCompatActivity() {
+class ReposActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.repos_activity)
     }
 }
